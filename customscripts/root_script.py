63046357b23b166a60a0d08ed483b6d697955854
kkthxbye <400797+kkthxbye-code@users.noreply.github.com>
2023-10-06 10:12:15 +0200

    Update root_script.py

--- customscripts/root_script.py
+++ customscripts/root_script.py
@@ -16,7 +16,7 @@ class RootScript1(CustomScript):
     )
 
     def run(self, data, commit):
-        self.log_success("RootScript13371337")
+        self.log_success("RootScript")
         self.log_success(shared_function())
 
 class RootScript2(CustomScript):
